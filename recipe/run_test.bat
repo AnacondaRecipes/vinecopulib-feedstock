d35e05869abc34bd96c955e6d7d405d78a54c438
Andrii-Osipov <aosipov@anaconda.com>
2025-04-17 03:39:32 -0700

    vinecopulib 0.7.1 (#2)
    * PKG-7994: vinecopulib 0.7.1

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -7,7 +7,8 @@
 cd examples/bicop
 
 :: /EHsc - https://stackoverflow.com/questions/6832666/lnk2019-when-including-asio-headers-solution-generated-with-cmake
-%CXX% ./src/main.cpp /EHsc /I %LIBRARY_INC% /I %LIBRARY_INC% /I %LIBRARY_INC%\eigen3 /I %LIBRARY_INC%\boost /I %LIBRARY_INC%\wdm  /I %LIBRARY_INC%\vinecopulib /link /LIBPATH:%LIBRARY_LIB%
+%CXX% ./src/main.cpp /EHsc /DBOOST_ALL_NO_LIB /I %LIBRARY_INC% /I %LIBRARY_INC%\eigen3 /I %LIBRARY_INC%\boost /I %LIBRARY_INC%\wdm /I %LIBRARY_INC%\vinecopulib /link /LIBPATH:%LIBRARY_LIB%
+
 if errorlevel 1 exit /B 1
 
 main.exe
